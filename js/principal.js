4275d11d1d2e9fdffcf109cb7dffdabb85b7fdd4
Gabriel <gabrielvieira840@gmail.com>
2019-09-19 14:16:06 -0300

    First

---
+++ js/principal.js
@@ -0,0 +1,7 @@
+
+var titulo = (document.querySelector(".titulo"));
+titulo.textContent = "IMC";
+var subtitulo = (document.querySelector(".subtitulo"));
+subtitulo.textContent = "Lista dos clientes";
+console.log (titulo);
+		
