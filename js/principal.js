dca2dca3a08058ae555d24f04cfe87507a16a3b0
Gabriel <gabrielvieira840@gmail.com>
2019-09-19 14:50:36 -0300

    Alterando IMC

--- js/principal.js
+++ js/principal.js
@@ -1,7 +1,45 @@
-
 var titulo = (document.querySelector(".titulo"));
-titulo.textContent = "IMC";
+    titulo.textContent = "IMC";
 var subtitulo = (document.querySelector(".subtitulo"));
-subtitulo.textContent = "Lista dos clientes";
-console.log (titulo);
+    subtitulo.textContent = "Lista dos clientes";
+
+var paciente = (document.querySelector("#primeiro-paciente"));
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+    console.log (paciente);
+    console.log (tdPeso);
+    console.log (peso); 
+
+if (peso <= 0 || peso >= 300)
+{
+    alert ("Peso informado incorretamente");
+    tdPeso.textContent = "Erro !";
+    altura = "N/A"
+}
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+    console.log(altura);
+
+if (altura < 0 || altura >= 3.00)
+{
+    alert ("Altura informada incorretamente");
+    tdAltura.textContent = "Erro !";
+    altura = "N/A"
+}
+
+var IMC = peso / (altura * altura);
+
+console.log (IMC);
+
+var imcPrimeiroPaciente = (document.querySelector(".info-imc"));
+    imcPrimeiroPaciente.textContent = IMC;
+if (imcPrimeiroPaciente.textContent == 'NaN')
+{
+    imcPrimeiroPaciente.textContent = "Não foi possivel calcular IMC";
+}
+
+
+
+
 		
