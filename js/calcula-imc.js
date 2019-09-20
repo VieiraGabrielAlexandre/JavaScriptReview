4ed9637c15b2c5aa2c3f2db40a256766a15381c9
Gabriel <gabrielvieira840@gmail.com>
2019-09-20 01:27:36 -0300

    Fracionando funções

--- js/calcula-imc.js
+++ js/calcula-imc.js
@@ -14,14 +14,14 @@ for (var i = 0; i < pacientes.length; i++) {
     var tdAltura = paciente.querySelector(".info-altura");
     var altura = tdAltura.textContent;
 
-    var imc = peso / (altura * altura);
+    var imc = calculaImc(peso, altura);
     var imcPaciente = paciente.querySelector(".info-imc");
     
     if (imc >= 60 || imc <= 0)
     {
         imcPaciente.textContent = "Ocorreu algum erro";
     } else {
-        imcPaciente.textContent = imc.toFixed(2);
+        imcPaciente.textContent = calculaImc(peso, altura);
     }
     
     if (peso <= 0 || peso >= 300) {
@@ -40,41 +40,13 @@ for (var i = 0; i < pacientes.length; i++) {
         imcPaciente.textContent = "Não foi possivel calcular IMC";
     }
 }
-titulo.addEventListener("click", function (){
-    console.log("Função anonomina");
-});
 
-var botaoAdicionar = document.querySelector("#adicionar-paciente");
-botaoAdicionar.addEventListener("click", function(event){
-    event.preventDefault();
-    
-    var form = document.querySelector("#form-adiciona");
-    var nome = form.nome.value;
-    var peso = form.peso.value;
-    var altura = form.altura.value;
-    var gordura = form.gordura.value;
+function calculaImc(peso, altura){
+    var imc = 0;
+    imc = peso / (altura * altura); 
 
-    var pacienteTr = document.createElement("tr");
-    var nomeTd = document.createElement("td");
-    var pesoTd = document.createElement("td");
-    var alturaTd = document.createElement("td");
-    var gorduraTd = document.createElement("td");
-    var imcTd = document.createElement("td");
-    
-    nomeTd.textContent = nome;
-    pesoTd.textContent = peso;
-    alturaTd.textContent = altura;
-    gorduraTd.textContent = gordura;
-
-    pacienteTr.appendChild(nomeTd);
-    pacienteTr.appendChild(pesoTd);
-    pacienteTr.appendChild(alturaTd);
-    pacienteTr.appendChild(gorduraTd);
-
-    var tabela = document.querySelector("#tabela-pacientes");
-    tabela.appendChild(pacienteTr);
-});
-
+    return imc.toFixed(2);
+}
 
 function mostraMensagem(){
     console.log("Ola!");
